5b5bdbb2ac97b36df97be27af0c61ed47f7336e9
adityajain23 <aditya.jain.19031@iitgoa.ac.in>
2021-08-12 13:02:42 +0530

    done

---
+++ app/screens/GameScreen.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  Image,
+  ImageBackground,
+} from "react-native";
+
+class GameScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <View>
+        <Text> GameScreen </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({});
+
+export default GameScreen;
