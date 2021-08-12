f13bdadf0804ed27a9d4420aa195b773da584aae
adityajain23 <aditya.jain.19031@iitgoa.ac.in>
2021-08-12 13:04:29 +0530

    done

---
+++ app/screens/MainScreen.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+class MainScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <View>
+        <Text> MainScreen </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({});
+
+export default MainScreen;
